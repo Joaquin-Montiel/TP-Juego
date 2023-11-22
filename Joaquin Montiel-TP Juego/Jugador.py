52c778419f8ea3be00fba965af60ab1fb45fa904
Joaquin-Montiel <joacomontiel94@gmail.com>
2023-11-22 08:05:27 -0300

    Add files via upload

---
+++ Joaquin Montiel-TP Juego/Jugador.py
@@ -0,0 +1,42 @@
+# import pygame
+
+# RUTA_DER = r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Still.png'
+# RUTA_IZ = r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Still(iz).png'
+
+# dinosaurio_camina_derecha = [
+#     pygame.image.load(r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Walk\Walk(1).png'),
+#     pygame.image.load(r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Walk\Walk(2).png'),
+#     pygame.image.load(r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Walk\Walk(3).png'),
+#     pygame.image.load(r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Walk\Walk(4).png'),
+#     pygame.image.load(r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Walk\Walk(5).png'),
+#     pygame.image.load(r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Walk\Walk(6).png'),
+#     pygame.image.load(r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Walk\Walk(7).png'),
+#     pygame.image.load(r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Walk\Walk(8).png'),
+#     pygame.image.load(r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Walk\Walk(9).png'),
+#     pygame.image.load(r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Walk\Walk(10).png')
+# ]
+
+# dinosaurio_camina_izquierda = [
+#     pygame.image.load(r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Walk\Walk(1)iz.png'),
+#     pygame.image.load(r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Walk\Walk(2)iz.png'),
+#     pygame.image.load(r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Walk\Walk(3)iz.png'),
+#     pygame.image.load(r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Walk\Walk(4)iz.png'),
+#     pygame.image.load(r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Walk\Walk(5)iz.png'),
+#     pygame.image.load(r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Walk\Walk(6)iz.png'),
+#     pygame.image.load(r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Walk\Walk(7)iz.png'),
+#     pygame.image.load(r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Walk\Walk(8)iz.png'),
+#     pygame.image.load(r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Walk\Walk(9)iz.png'),
+#     pygame.image.load(r'C:\Users\joaqu\OneDrive\Desktop\Joaquin Montiel-TP Juego\sprites_juego\Walk\Walk(10)iz.png')
+# ]
+
+
+# class Jugador(pygame.sprite.Sprite):
+#     def __init__(self, nombre:str, vidas:int, dinosaurio_derecha, dinosaurio_izquierda, dinosaurio_camina_derecha, dinosaurio_camina_izquierda) -> None:
+#         super().__init__()
+#         self.nombre =  input(" Ingrese el nombre del dino")
+#         self.vidas = 3
+#         self.dinosaurio_derecha = pygame.image.load(RUTA_DER)
+#         self.dinosaurio_izquierda = pygame.image.load(RUTA_IZ)
+#         self.dinosaurio_camina_derecha = dinosaurio_camina_derecha
+#         self.dinosaurio_camina_izquierda = dinosaurio_camina_izquierda
+
